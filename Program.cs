da4a91d56e350c9188d77d31e308b76dc7ac2828
barmao <bryanbarmao@gmail.com>
2021-08-17 23:41:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SRService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
